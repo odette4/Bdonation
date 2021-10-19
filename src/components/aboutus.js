87371c88f5a94a10e538cafaf29caf4c97914858
odette <odethdusabe@gmail.com>
2021-10-19 22:21:54 +0200

    blood donation

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -5,7 +5,13 @@ const Aboutus=()=>{
     return(
         <div>
             <Header/>
+
+            <p>Blood bank is a place where blood bag is collected from blood donation
+            events is stored in one place. the term "blood bank" refers to adivision of a hospital laboratory
+            where the storage of blood product occurs and where proper testing is performed to reduce the task</p>
         </div>
+
+        
     )
 }
 
